8121cb79ac37897eac34861de610a8b94770b26c
DuboisCorentin <112875903+DuboisCorentin@users.noreply.github.com>
2024-02-28 10:52:43 +0100

    server fastify

---
+++ tp5/src/server.js
@@ -0,0 +1,52 @@
+import Fastify from "fastify"
+import fastifyBasicAuth from "@fastify/basic-auth"
+
+
+const port = 3000;
+const authenticate = {realm: 'Westeros'}
+
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.register(fastifyBasicAuth, {
+    validate,
+    authenticate
+})
+
+async function validate(username, password, req, reply) {
+    if (username !== 'Corentin' || password !== 'dubois') {
+        return new Error('Error')
+    }
+}
+
+fastify.after(() => {
+    fastify.route({
+        method: 'GET',
+        url: '/secu',
+        onRequest: fastify.basicAuth,
+        handler: async (req, reply) => {
+            return {
+                replique: 'Connecté !'
+            }
+        }
+    })
+})
+
+fastify.setErrorHandler(function (err, req, reply) {
+
+    if (err.statusCode === 401) {
+        console.log(err)
+        reply.code(401).send({replique: 'Erreur de connexion'})
+    }
+    reply.send(err)
+})
+
+fastify.listen({port}, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+
+    fastify.log.info(`Fastify is listening on port: ${address}`);
+});
